72aa74a78c4415b2c72926145f7737821c1830be
valex <vasilica.hutanu@e-uvt.ro>
2022-12-03 15:33:06 +0200

    Create desktop navbar

--- src/components/DesktopNavBar/DesktopNavBar.tsx
+++ src/components/DesktopNavBar/DesktopNavBar.tsx
@@ -1,7 +1,52 @@
+import { useNavigate } from 'react-router-dom'
+import BoardIcon from '../../icons/BoardIcon';
+import ProjectIcon from '../../icons/ProjectIcon'
+import TicketIcon from '../../icons/TicketIcon'
+
 export default () => {
+
+   const navigate = useNavigate()
+
    return <>
       <div className="desktop-nav-bar">
-         <p>NavBar</p>
+         <div className="desktop-nav-bar__upper-section">
+            <p className="desktop-nav-bar__upper-section__title" onClick={() => navigate('/homepage')}>
+               Workflow manager
+            </p>
+         </div>
+         <div className="desktop-nav-bar__lower-section">
+            <div className="desktop-nav-bar__lower-section__profile">
+               <div className="desktop-nav-bar__lower-section__profile__picture">
+                  <img src="profile_picture.jpg" alt="profile picture"/>
+               </div>
+               <p className="desktop-nav-bar__lower-section__profile__name">
+                  Alex Hutanu
+               </p>
+               <p className="desktop-nav-bar__lower-section__profile__mail-address">
+                  alexandru.hutanu@amdaris.com
+               </p>
+            </div>
+            <div className="desktop-nav-bar__lower-section__links">
+               <div className="desktop-nav-bar__lower-section__links__element">
+                  <BoardIcon />
+                  <p className="desktop-nav-bar__lower-section__links__element__name">
+                     Dashboard
+                  </p>
+               </div>
+               <div className="desktop-nav-bar__lower-section__links__element">
+                  <ProjectIcon />
+                  <p className="desktop-nav-bar__lower-section__links__element__name">
+                     Projects
+                  </p>
+               </div>
+               <div className="desktop-nav-bar__lower-section__links__element">
+                  <TicketIcon />
+                  <p className="desktop-nav-bar__lower-section__links__element__name">
+                     Tickets
+                  </p>
+               </div>
+            </div>
+         </div>
       </div>
    </>
 }
